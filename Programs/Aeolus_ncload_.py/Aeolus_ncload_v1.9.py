437f3dd80bc0a3017c49447251b599de4f7a862d
Tim Banyard <tpb38@ubpc-2307.bath.ac.uk>
2020-04-24 12:14:51 +0100

    Friday update to ncload for Andes ERA5 perturbation plot

--- Programs/Aeolus_ncload_.py/Aeolus_ncload_v1.9.py
+++ Programs/Aeolus_ncload_.py/Aeolus_ncload_v1.9.py
@@ -60,7 +60,7 @@ os.chdir('..')
 # Here I need to iterate through all. nc files and plot all of them
 # into jpgs to view one after another
 """Find directory and read Aeolus netCDF data"""
-strdirectory = '/home/tpb38/PhD/Bath/Aeolus/NC_FullQC/'
+strdirectory = '/home/tpb38/PhD/Bath/Aeolus/NC4_FullQC/'
 """ERA5 directory"""
 ERA5_dir = '/home/tpb38/PhD/Bath/ERA5/'
 
@@ -77,6 +77,7 @@ vert_res = 500 # Given in metres
 # Azores or Andes? (Comment in the desired region)
 region = 'andes'
 # ~ region = 'azores'
+
 if region == 'andes':
 	minlat, maxlat, minlon, maxlon = -80, -40, 280, 320
 	bmlowlat, bmupperlat, bmleftlon, bmrightlon = -80, -40, -80, -40
@@ -116,6 +117,13 @@ for file in os.listdir(directory):
 	# ~ data_HLOS = data.variables['Zonal_wind_projection'][:]
 	# ~ data_HLOS = data.variables['Meridional_wind_projection'][:]
 	# ~ data_HLOS = data.variables['LOS_azimuth'][:]
+	# Zonal Wind Projection
+	data_u_wind = data.variables['Zonal_wind_projection'][:]
+	# Meridional Wind Projection
+	data_v_wind = data.variables['Meridional_wind_projection'][:]
+	# Line-of-sight azimuth
+	data_azimuth = data.variables['LOS_azimuth'][:]
+		
 	# Rayleigh_Grouping
 	RG = data.variables['RG'][:]
 	# Time
@@ -177,6 +185,9 @@ for file in os.listdir(directory):
 		data_alt_new = data_alt[start_elmnt:end_elmnt+1]
 		data_HLOS_new = data_HLOS[start_elmnt:end_elmnt+1]
 		rayleigh_times_new = rayleigh_times[start_elmnt:end_elmnt+1]
+		data_u_wind = data_u_wind[start_elmnt:end_elmnt+1]
+		data_v_wind = data_v_wind[start_elmnt:end_elmnt+1]
+		data_azimuth = data_azimuth[start_elmnt:end_elmnt+1]
 
 		"""=========================================================="""
 		"""=====================ERA5 Interpolation==================="""
@@ -231,18 +242,63 @@ for file in os.listdir(directory):
 		# ==============================================================
 		
 		# =====================Smooth u and v fields====================
-		# ~ print(np.shape(ERA5_data_u)
-		ERA5_data_u = ndimage.gaussian_filter1d(ERA5_data_u, 17.5, axis = 1)
-		ERA5_data_u = ndimage.gaussian_filter1d(ERA5_data_u, 0.5, axis = 2)
-		ERA5_data_u = ndimage.gaussian_filter1d(ERA5_data_u, 0.5, axis = 3)
-		ERA5_data_v = ndimage.gaussian_filter1d(ERA5_data_v, 17.5, axis = 1)
-		ERA5_data_v = ndimage.gaussian_filter1d(ERA5_data_v, 0.5, axis = 2)
-		ERA5_data_v = ndimage.gaussian_filter1d(ERA5_data_v, 0.5, axis = 3)
+		print(np.shape(ERA5_data_u))
+		
+		# Here, altdiff, latdiff and londiff give the fwhm window size in km.
+		# From this, the fwhm window size in points is found and converted to sd
+		
+		# Calculate latitude difference
+		lats = np.deg2rad(np.array([-50,-51.5]))
+		lons = np.deg2rad(np.array([-70,-70]))
+		latdiff = haversine(lats, lons)
+		lat_sig = (3 / latdiff) / 2.355
+		print("lat_sig: ", lat_sig)
+		
+		# Calculate longitude difference
+		lats = np.deg2rad(np.array([-50,-50]))
+		lons = np.deg2rad(np.array([-70,-71.5]))
+		londiff = haversine(lats, lons)
+		lon_sig = (3 / londiff) / 2.355
+		print("lon_sig: ", lon_sig)
+		
+		ERA5_data_u = ndimage.gaussian_filter1d(ERA5_data_u, lat_sig, axis = 2)
+		ERA5_data_u = ndimage.gaussian_filter1d(ERA5_data_u, lon_sig, axis = 3)
+		ERA5_data_v = ndimage.gaussian_filter1d(ERA5_data_v, lat_sig, axis = 2)
+		ERA5_data_v = ndimage.gaussian_filter1d(ERA5_data_v, lon_sig, axis = 3)
+		
+		new_ERA5_data_u = np.copy(ERA5_data_u)
+		new_ERA5_data_v = np.copy(ERA5_data_v)
+		
+		# Iterate through model levels and find alt difference with next level
+		for lev in range(111):
+			if lev != 136:
+				altdiff = ERA5_altitudes[lev] - ERA5_altitudes[lev+1]
+			elif lev == 136:
+				altdiff = ERA5_altitudes[lev]
+			alt_sig = (10000 / altdiff) / 2.355
+			print(lev + 1)
+			print(alt_sig)
+			
+			if lev != 110:
+				temp_ERA5_data_u = np.copy(ndimage.gaussian_filter1d(ERA5_data_u, alt_sig, axis = 1))
+				new_ERA5_data_u[:, lev, :, :] = temp_ERA5_data_u[:, lev, :, :]
+				temp_ERA5_data_v = np.copy(ndimage.gaussian_filter1d(ERA5_data_v, alt_sig, axis = 1))
+				new_ERA5_data_v[:, lev, :, :] = temp_ERA5_data_v[:, lev, :, :]
+			if lev == 110:
+				temp_ERA5_data_u = np.copy(ndimage.gaussian_filter1d(ERA5_data_u, alt_sig, axis = 1))
+				new_ERA5_data_u[:, 110:-1, :, :] = temp_ERA5_data_u[:, 110:-1, :, :]
+				temp_ERA5_data_v = np.copy(ndimage.gaussian_filter1d(ERA5_data_v, alt_sig, axis = 1))
+				new_ERA5_data_v[:, 110:-1, :, :] = temp_ERA5_data_v[:, 110:-1, :, :]
+		
+		ERA5_data_u = new_ERA5_data_u
+		ERA5_data_v = new_ERA5_data_v
 				
 		# ==============================================================
 				
 		# =======================Run Interpolation======================
 		# Create a datetime format version of rayleigh_times_new
+		ERA5_u_interpolated = np.empty(0)
+		ERA5_v_interpolated = np.empty(0)
 		ERA5_interpolated = np.empty(0)
 		no_result = 0
 		rayleigh_times_new_dt = \
@@ -256,56 +312,75 @@ for file in os.listdir(directory):
 			down_elmt = np.where(ERA5_data_time == rounded_down)
 			up_elmt = np.where(ERA5_data_time == rounded_up)
 			
-			# Toggle between ERA5 sandwich times at 3hr intervals
-			# and interpolate between them
-			for t_E in [down_elmt, up_elmt]:
-				t_E = t_E[0][0] # t_E = ERA5 time element
-				if t_E == down_elmt[0][0]:
-					time_toggle = 0
-				elif t_E == up_elmt[0][0]:
-					time_toggle = 1
-				else:
-					print("Error")
-				# Call run_interpolation to execute 3D spatial
-				# interpolation of ERA5 data onto Aeolus track
-				ERA5_var = ERA5_data_u
-				result = run_interpolation(data_lat_new_ae,
-				data_lon_new_ae, data_alt_new_ae, 
-				ERA5_data_lat, ERA5_data_lon, ERA5_altitudes, ERA5_var, t, t_E)
-				# Raise flag if there is no result
-				if result == None:
-					no_result = 1
+			for each in ['u', 'v', 'hlos']:
+				# If 'hlos' then we have already got a u and v value so:
+				if each == 'hlos':
+					hlos = (-ERA5_u_interpolated[t] * np.sin(data_azimuth[t] * (np.pi/180))) - \
+						(ERA5_v_interpolated[t] * np.cos(data_azimuth[t] * (np.pi/180)))
+					ERA5_interpolated = np.append(ERA5_interpolated, hlos)
+					# Now we skip the rest of the loop
 					continue
-				# Set up and down times
-				if time_toggle == 0:
-					down = result
-					# ~ print("Down = ", down[0])			
-				elif time_toggle == 1:
-					up = result
-					# ~ print("Up = ", up[0])
+					
+				# Toggle between ERA5 sandwich times at 3hr intervals
+				# and interpolate between them
+				for t_E in [down_elmt, up_elmt]:
+					t_E = t_E[0][0] # t_E = ERA5 time element
+					if t_E == down_elmt[0][0]:
+						time_toggle = 0
+					elif t_E == up_elmt[0][0]:
+						time_toggle = 1
+					else:
+						print("Error")
+						
+					# Call run_interpolation to execute 3D spatial
+					# interpolation of ERA5 data onto Aeolus track
+					if each == 'u':
+						ERA5_var = ERA5_data_u
+					elif each == 'v':
+						ERA5_var = ERA5_data_v
+					result = run_interpolation(data_lat_new_ae,
+					data_lon_new_ae, data_alt_new_ae, 
+					ERA5_data_lat, ERA5_data_lon, ERA5_altitudes, ERA5_var, t, t_E)
+					# Raise flag if there is no result
+					if result == None:
+						no_result = 1
+						continue
+					# Set up and down times
+					if time_toggle == 0:
+						down = result
+						# ~ print("Down = ", down[0])			
+					elif time_toggle == 1:
+						up = result
+						# ~ print("Up = ", up[0])
+					if no_result == 1:
+						break
+				
+				# Break out of loop if there is no result		
 				if no_result == 1:
-					break
+					if each == 'u':
+						ERA5_u_interpolated = np.append(ERA5_u_interpolated, 99999)
+					elif each == 'v':
+						ERA5_v_interpolated = np.append(ERA5_v_interpolated, 99999)
+					no_result = 0
+					continue
+				
+				# Linear interpolation in time using linearinterp2kp
+				# function
+				x_0 = toTimestamp(ERA5_data_time[down_elmt[0][0]])
+				x_1 = toTimestamp(ERA5_data_time[up_elmt[0][0]])
+				x = toTimestamp(rayleigh_times_new_dt[t])
+				y_0 = down[0]
+				y_1 = up[0]
+				y = linearinterp2kp(x, x_0, x_1, y_0, y_1)
+				
+				if each == 'u':
+					ERA5_u_interpolated = np.append(ERA5_u_interpolated, y)
+				elif each == 'v':
+					ERA5_v_interpolated = np.append(ERA5_v_interpolated, y)
 			
-			# Break out of loop if there is no result		
-			if no_result == 1:
-				ERA5_interpolated = np.append(ERA5_interpolated, 99999)
-				no_result = 0
-				continue
-			
-			# Linear interpolation in time using linearinterp2kp
-			# function
-			x_0 = toTimestamp(ERA5_data_time[down_elmt[0][0]])
-			x_1 = toTimestamp(ERA5_data_time[up_elmt[0][0]])
-			x = toTimestamp(rayleigh_times_new_dt[t])
-			y_0 = down[0]
-			y_1 = up[0]
-			y = linearinterp2kp(x, x_0, x_1, y_0, y_1)
-			
-			ERA5_interpolated = np.append(ERA5_interpolated, y)
-
 		# ~ print(len(ERA5_interpolated))
 		# ~ print(len(rayleigh_times_new))
-		data_HLOS_new = np.copy(ERA5_interpolated)*-100
+		# ~ data_HLOS_new = np.copy(ERA5_interpolated)*100 
 		# ~ print(data_HLOS_new)
 		"""=========================================================="""
 		"""=================Creating arrays for plot================="""
@@ -315,87 +390,92 @@ for file in os.listdir(directory):
 		maxheight = 20000
 		levnum = (maxheight / vert_res) + 1
 		alts = np.linspace(0,maxheight, levnum)
-		# ~ #Lists
-		# ~ z = [[0 for _ in range(len(RG))] for _ in range(len(alts))]
-		# ~ z_itrn = \
-		# ~ [[0 for _ in range(len(RG))] for _ in range(len(alts))]
-		z = np.zeros((len(alts),len(RG))) # NumPy Arrays
-		z_itrn = np.zeros((len(alts),len(RG)))
-		# ~ print(np.shape(z))
 		
-		# ============OLD INTERPOLATION ROUTINE==============
-		if oldinterp == True:
-			# Placing wind values into bins of height 1km and width
-			# 1 rayleigh group
-			lastgroupstarttime = 0
-			RG_start = 0
-			for RG_elmnt in range(len(RG)):
-				for t in range(len(rayleigh_times_new)):
-					# Find all elements inside this sandwich and add to z
-					# and z_itrn:
-					if rayleigh_times_new[t] < RG[RG_elmnt] and \
-					rayleigh_times_new[t] >= lastgroupstarttime:
-						if RG_start == 0:
-							RG_start = RG_elmnt
-						# Find the nearest altitude level
-						val = find_nearest(alts, data_alt_new[t])
-						alt_elmnt = np.where(alts == val)[0][0]
-						# Cap wind speeds to 250 m/s
-						if np.abs(data_HLOS_new[t]) < 25000:
-							z[alt_elmnt][RG_elmnt] += data_HLOS_new[t]
-							z_itrn[alt_elmnt][RG_elmnt] += 1
-						RG_end = RG_elmnt
-				lastgroupstarttime = RG[RG_elmnt]
-		# ===================================================			
-		else:
-		# ============NEW INTERPOLATION ROUTINE==============
-			z_new = np.zeros((len(alts),len(RG))) # NumPy Arrays
-			points = np.empty(0)
-			values = np.empty(0)
-			lastgroupstarttime = 0
-			RG_start = 0
-			for RG_elmnt in range(len(RG)):
-				for t in range(len(rayleigh_times_new)):
-					# Find all elements inside this sandwich and add to z
-					# and z_itrn:
-					if rayleigh_times_new[t] < RG[RG_elmnt] and \
-					rayleigh_times_new[t] >= lastgroupstarttime:
-						if RG_start == 0:
-							RG_start = RG_elmnt
-						# Cap wind speeds to 250 m/s
-						if np.abs(data_HLOS_new[t]) < 25000:
-							if data_alt_new[t] in points:
-								itis = np.where(points == data_alt_new[t])
-							points = np.append(points, data_alt_new[t])
-							values = np.append(values, data_HLOS_new[t])
-						RG_end = RG_elmnt
-				lastgroupstarttime = RG[RG_elmnt]
-				z_new[:, RG_elmnt] = griddatainterpolation(points, values, alts)
+		# Detrending to the smoothed model
+		data_obs = np.copy(data_HLOS_new) # Store observations for later use
+		data_mod = np.copy(ERA5_interpolated)*100 # Converts ERA5 data into cm/s
+		for each in ['mod', 'obs']:
+			if each == 'mod':
+				data_HLOS_new = data_mod
+			elif each == 'obs':
+				data_HLOS_new = data_obs
+		
+			# ~ #Lists
+			# ~ z = [[0 for _ in range(len(RG))] for _ in range(len(alts))]
+			# ~ z_itrn = \
+			# ~ [[0 for _ in range(len(RG))] for _ in range(len(alts))]
+			z = np.zeros((len(alts),len(RG))) # NumPy Arrays
+			z_itrn = np.zeros((len(alts),len(RG)))
+			# ~ print(np.shape(z))
+			
+			# ============OLD INTERPOLATION ROUTINE==============
+			if oldinterp == True:
+				# Placing wind values into bins of height 1km and width
+				# 1 rayleigh group
+				lastgroupstarttime = 0
+				RG_start = 0
+				for RG_elmnt in range(len(RG)):
+					for t in range(len(rayleigh_times_new)):
+						# Find all elements inside this sandwich and add to z
+						# and z_itrn:
+						if rayleigh_times_new[t] < RG[RG_elmnt] and \
+						rayleigh_times_new[t] >= lastgroupstarttime:
+							if RG_start == 0:
+								RG_start = RG_elmnt
+							# Find the nearest altitude level
+							val = find_nearest(alts, data_alt_new[t])
+							alt_elmnt = np.where(alts == val)[0][0]
+							# Cap wind speeds to 250 m/s
+							if np.abs(data_HLOS_new[t]) < 25000:
+								z[alt_elmnt][RG_elmnt] += data_HLOS_new[t]
+								z_itrn[alt_elmnt][RG_elmnt] += 1
+							RG_end = RG_elmnt
+					lastgroupstarttime = RG[RG_elmnt]
+			# ===================================================			
+			else:
+			# ============NEW INTERPOLATION ROUTINE==============
+				z_new = np.zeros((len(alts),len(RG))) # NumPy Arrays
 				points = np.empty(0)
 				values = np.empty(0)
-		# ===================================================
-							
-		# Find the mean for each bin (Toggled on for old interpolation)
-		if oldinterp == True:
-			z /= 100 * z_itrn # Factor of 100 for conversion from cm/s - m/s
-		else:
-			# Using new interpolation routine
-			z = np.copy(z_new) / 100
-				
-		# Amend RG array
-		RG_new = RG[RG_start:RG_end+1]
-		z = z[:, RG_start:RG_end+1]
-		
-		# Fixing time dimension to match coda format
-		date_time = coda.time_to_utcstring(RG_new[:])
-		date_time = np.array([datetime.strptime(date,
-			'%Y-%m-%d %H:%M:%S.%f') for date in date_time])
-		rayleigh_times_new = \
-		coda.time_to_utcstring(rayleigh_times_new[:])
-		rayleigh_times_new = np.array([datetime.strptime(date,
-			'%Y-%m-%d %H:%M:%S.%f') for date in rayleigh_times_new])
-		x, y = np.meshgrid(date_time, alts)
-		
+				lastgroupstarttime = 0
+				RG_start = 0
+				for RG_elmnt in range(len(RG)):
+					for t in range(len(rayleigh_times_new)):
+						# Find all elements inside this sandwich and add to z
+						# and z_itrn:
+						if rayleigh_times_new[t] < RG[RG_elmnt] and \
+						rayleigh_times_new[t] >= lastgroupstarttime:
+							if RG_start == 0:
+								RG_start = RG_elmnt
+							# Cap wind speeds to 250 m/s
+							if np.abs(data_HLOS_new[t]) < 25000:
+								if data_alt_new[t] in points:
+									itis = np.where(points == data_alt_new[t])
+								points = np.append(points, data_alt_new[t])
+								values = np.append(values, data_HLOS_new[t])
+							RG_end = RG_elmnt
+					lastgroupstarttime = RG[RG_elmnt]
+					z_new[:, RG_elmnt] = griddatainterpolation(points, values, alts)
+					points = np.empty(0)
+					values = np.empty(0)
+			# ===================================================
+								
+			# Find the mean for each bin (Toggled on for old interpolation)
+			if oldinterp == True:
+				z /= 100 * z_itrn # Factor of 100 for conversion from cm/s - m/s
+			else:
+				# Using new interpolation routine
+				z = np.copy(z_new) / 100
+					
+			# Amend RG array
+			RG_new = RG[RG_start:RG_end+1]
+			z = z[:, RG_start:RG_end+1]
+			
+			if each == 'mod':			
+				data_mod = np.copy(z)
+			elif each == 'obs':
+				data_obs = np.copy(z)
+	
 		# Applying NaN mask
 		grayhatchescmap = LinearSegmentedColormap('Grayhatchescmap',
 			segmentdata=customcolormaps('grayhatches'), N=265)
@@ -408,6 +488,16 @@ for file in os.listdir(directory):
 				else:
 					hatcharray[horidx][veridx] = 0
 		
+		# Fixing time dimension to match coda format
+		date_time = coda.time_to_utcstring(RG_new[:])
+		date_time = np.array([datetime.strptime(date,
+			'%Y-%m-%d %H:%M:%S.%f') for date in date_time])
+		rayleigh_times_new = \
+		coda.time_to_utcstring(rayleigh_times_new[:])
+		rayleigh_times_new = np.array([datetime.strptime(date,
+			'%Y-%m-%d %H:%M:%S.%f') for date in rayleigh_times_new])
+		x, y = np.meshgrid(date_time, alts)
+					
 		"""=========================================================="""
 		"""=======================Plotting==========================="""
 		"""=========================================================="""
@@ -447,6 +537,8 @@ for file in os.listdir(directory):
 			y_lim_max = (maxheight + (vert_res/2)) / 1000
 			y_lim_min = (0 - (vert_res/2)) / 1000
 			y_lims = [y_lim_max, y_lim_min]
+			
+			# ======================= Pre ERA5 Scheme ======================== #
 			fixnanswithmean(z) # Uses my own function 'fixnanswithmean'	
 			
 			# Test using Gaussian noise
@@ -454,7 +546,7 @@ for file in os.listdir(directory):
 				# ~ for j in range(len(z[0])):
 					# ~ z[i][j] = random.gauss(0, 20)
 			
-			# ~ z = ndimage.uniform_filter(z, size=(3,7), mode = 'reflect')
+			# ~ z = ndimage.uniform_filter(z, size=(3,7), mode = 'reflect') #N/A
 			z1 = np.copy(z)
 			# Calculate appropriate upper and lower bounds to band-pass
 			sg_upper = int(np.ceil(15 * (1000/vert_res)))
@@ -466,12 +558,17 @@ for file in os.listdir(directory):
 			z2 = savgol_filter(z, sg_upper, 2, axis = 0) # + Vertical S-G filter
 			z3 = savgol_filter(z, sg_lower, 2, axis = 0) # - Vertical S-G filter
 			# ~ z2 = savgol_filter(z, 15, 2, axis = 1) # Horizontal S-G filter
-			z = z3 - z2
+			z = z3 - z2 # Return the perturbations
 			# ~ z = z1 - z2
+			# ======================= Pre ERA5 Scheme ======================== #
+			# ~ fixnanswithmean(data_obs)
+			# ~ z = data_obs - data_mod # Find perturbations relative to smoothed ERA5
+			z = data_mod
+			# ~ z = data_obs
 						
 			# Plots using imshow
 			cs = plt.imshow(z, aspect='auto', cmap='RdBu_r', extent=[x_lims[0],
-				x_lims[1], y_lims[0], y_lims[1]], vmin=-10, vmax=10,
+				x_lims[1], y_lims[0], y_lims[1]], vmin=-150, vmax=150,
 				interpolation='none')
 			mask = plt.imshow(hatcharray, aspect='auto', cmap=grayhatchescmap,
 				extent=[x_lims[0], x_lims[1], y_lims[0], y_lims[1]], interpolation=im_interp)
@@ -628,7 +725,7 @@ for file in os.listdir(directory):
 		# Climb out of plot directory
 		os.chdir('..')
 		os.chdir('..')
-		plt.savefig('testme.png',dpi=300)
+		plt.savefig('testme7_10kmmod.png',dpi=300)
 		
 		# Time taken for the file
 		fduration = datetime.now() - fstartTime
